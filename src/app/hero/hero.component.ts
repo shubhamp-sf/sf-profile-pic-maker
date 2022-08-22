3995d3b352279a70a1be703a3d7de9965184f4d6
Shubham Prajapat <shubhamp.developer@gmail.com>
2022-08-22 22:47:21 +0530

    style: hero section, header button, background

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  handleUploadClick() {
+    console.log('Upload Clicked');
   }
-
 }
