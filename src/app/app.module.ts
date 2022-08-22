ae6acfb3e2fb666b73fe3109dcea4f8eef3fbfd2
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-08-22 21:55:58 +0530

    style: hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContainerComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
